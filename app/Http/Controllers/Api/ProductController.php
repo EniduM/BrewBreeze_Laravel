a71d7d34a02be56c7517691892d85fc6a3ba0a88
MALUDDENIYE GEDARA ENIDU OUSHANA MALUDDENIYA CB012968 <maluddeniyaeniduoushana@gmail.com>
2026-02-07 23:10:58 +0530

    Update index method for paginated product retrieval
    Refactor index method to support pagination for products.

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -6,14 +6,9 @@ use App\Http\Controllers\Controller;
 use App\Models\Product;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
-use App\Http\Resources\ProductResource;
 
 class ProductController extends Controller
 {
-    public function index()
-    {
-        return ProductResource::collection(Product::all());
-    }
     /**
      * Get all available products with pagination.
      */
